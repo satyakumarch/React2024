c2da0e597714d59aefb429ef457d138c3fe70f9f
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-10-10 21:31:16 +0530

    react start with codevolution

---
+++ React/hello-world/src/component/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//  function Greet(){
+//     return
+//      <><h1>Hello World</h1>
+//      <h1>how are you</h1></>
+//  }
+  export const Greet=()=> <h1>Hello World Lovely professsional university</h1>
+
+// export default Greet;
