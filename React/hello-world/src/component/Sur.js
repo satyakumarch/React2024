fa52a684d76a398acacee328b152494f9dde2165
Satyakumarch <satyakumarchaudhary603@gmail.com>
2024-12-03 14:56:21 +0530

    endterm code

--- React/hello-world/src/component/Sur.js
+++ React/hello-world/src/component/Sur.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Surendra = () => {
     return (
         <div>
-            <h1>Hello, this is Surendra!</h1>
+            <h1>Hello, this is Satya kumar Chuadhary!</h1>
             <p>Welcome to my React component.</p>
             <h1>Lovely Professional university</h1>
         </div>
